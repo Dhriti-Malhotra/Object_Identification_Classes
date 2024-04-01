6d0c5d47fc22032fd0b620c2edcb71b2adf6b689
Dhriti-Malhotra <134493853+Dhriti-Malhotra@users.noreply.github.com>
2024-04-01 18:10:02 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+img = "";
+
+function preLoad()
+{
+
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420)
+    fill("a7fcef")
+    text("Dog", 45, 75);
+    noFill();
+    stroke("#a7e6fc");
+    rect(30, 60, 450, 350);
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+}
