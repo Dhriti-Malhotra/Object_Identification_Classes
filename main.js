e8ba3120f4ea256fa49b651f4e0d2b89a0b18970
Dhriti-Malhotra <134493853+Dhriti-Malhotra@users.noreply.github.com>
2024-04-02 17:51:39 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 img = "";
+status = "";
 
 function preLoad()
 {
@@ -7,16 +8,40 @@ function preLoad()
 
 function draw()
 {
-    image(img, 0, 0, 640, 420)
-    fill("a7fcef")
+    image(img, 0, 0, 640, 420);
+    fill("a7fcef");
     text("Dog", 45, 75);
     noFill();
     stroke("#a7e6fc");
     rect(30, 60, 450, 350);
+
+    fill("#a7fcef");
+    text("Cat", 320, 120)
+    noFill();
+    stroke("#a7e6fc");
+    rect(300, 90, 270, 320);
 }
 
 function setup()
 {
     canvas = createCanvas(640, 420);
     canvas.center();
+    objetDetector = ml5.objectDetector('cocossd', modelLoaded)
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
 }
